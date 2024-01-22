5a3c54a5e8dad0dc93218a7d736f873c5a46983e
Pavel Pavlenko <136743498+areallypawa@users.noreply.github.com>
2024-01-22 20:09:51 +0300

    Загрузка
    Крч школьный мини-проект для расчета выбросов СО2 от сжигания топлива. По сути простая практика JS

---
+++ sourse/JS/calc_20180701.js
@@ -0,0 +1,14 @@
+
+function calculatorco2() {
+	fuelInd = document.getElementById("fuel").options.selectedIndex;
+	fuelPar = document.getElementById("fuel").options[fuelInd].value;
+	q = eval(document.getElementById("q").value);
+	
+	measure = "т у.т.";
+	koef = parseFloat(fuelPar.split('-')[1]);
+	total = koef*q;
+	
+	document.getElementById("koefTxt").innerHTML = koef.toFixed(3);
+	document.getElementById("measure").innerHTML = measure;
+	document.getElementById("total").innerHTML = total.toFixed(3);
+}
